34c9695777170bb51dc25009dad45de3fa05e210
Piyushrajsaaa <raaz66006@gmail.com>
2024-10-22 17:54:37 +0530

    hello chambalwalon!

---
+++ wf.py
@@ -0,0 +1,5 @@
+n=int(input("enter the no"))
+s=0
+for i in n:
+    s=(n*(n+1))/2
+    
---
+++ wrfgr.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int i,n=6,s=0;
+    for(i=0;i<=n;i++){
+        s=(n*(n+1))/2;
+        
+    }
+    printf("%d",s);
+    return 0;
+}
